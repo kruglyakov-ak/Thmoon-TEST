5ebc90ef676604ef343b701a8f2805c61bdc7508
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2022-04-08 22:03:13 +0300

    Установил React router dom

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { AppRoutes } from '../../const';
 import GlobalStyle from '../../theme/globalStyle';
 import theme from '../../theme/theme';
+import MainPage from '../main-page/main-page';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <div>
-        <h1>Кинотеатр</h1>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path={AppRoutes.Main} element={<MainPage />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
